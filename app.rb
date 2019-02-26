9cfe02d3363d6f69188a929c10e7fd5be1a9ed97
Paul Ashour <paul.ashour@gmail.com>
2019-02-26 15:40:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   
   get '/' do
   end
